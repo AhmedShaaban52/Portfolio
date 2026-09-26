b9c0cb142fd53f3baf9088ad1ff52fbf2a4b80d6
Ahmed Shaaban <111895280+AhmedShaaban52@users.noreply.github.com>
2026-09-26 12:59:31 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import dynamic from "next/dynamic";
 import Hero from "@/components/Hero/Hero";
 import Section from "@/components/AboutMe/Section";
 
-const Skills = dynamic(() => import("@/components/Skills/Skills"), {ssr: false,});
-const MyProjects = dynamic(() => import("@/components/MyProjects/MyProjects"), {ssr: false,});
-const Contact = dynamic(() => import("@/components/Contact/Contact"), {ssr: false,});
+const Skills = dynamic(() => import("@/components/Skills/Skills"));
+const MyProjects = dynamic(() => import("@/components/MyProjects/MyProjects"));
+const Contact = dynamic(() => import("@/components/Contact/Contact"));
 
 export default function Home() {
   return (
